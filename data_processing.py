bdfc4a7180620c54f9983750fb938a14d3b4d0a1
Vedashree25 <140238993+Vedashree25@users.noreply.github.com>
2024-09-01 13:06:30 +0530

    Update data_processing.py

--- data_processing.py
+++ data_processing.py
@@ -1,9 +1,11 @@
 import pandas as pd
 
 def load_data(file_path: str):
-    if file_path.endswith('C:\Desktop\AI_Prototype\Iris.csv'):
+    file_path = r'C:\Desktop\AI_Prototype\Iris.csv'
+
+    if file_path.endswith(r'C:\Desktop\AI_Prototype\Iris.csv'):
         return pd.read_csv(file_path)
-    elif file_path.endswith('C:\Desktop\AI_Prototype\global.json'):
+    elif file_path.endswith(r'C:\Desktop\AI_Prototype\global.json'):
         return pd.read_json(file_path)
     elif file_path.endswith('.xlsx'):
         return pd.read_excel('Sample - Superstore.xls')
